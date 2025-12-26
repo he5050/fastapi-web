3fc5bfdad2bbe392ba8bd7c8c124176a9a30dd25
he <476532333@qq.com>
2025-12-26 10:22:56 +0800

    feat: 在基础Schema中添加时间字段序列化功能，统一时间格式输出

--- app/schemas/base_schema.py
+++ app/schemas/base_schema.py
@@ -1,12 +1,26 @@
-from pydantic import BaseModel, ConfigDict
+from typing import Any
+from datetime import datetime, date
+from pydantic import BaseModel, ConfigDict, field_serializer
 from pydantic.alias_generators import to_camel
 
 
 class BaseSchema(BaseModel):
     """
-    基础 Schema，支持蛇形转驼峰
+    基础 Schema，支持蛇形转驼峰及统一时间格式
     """
 
     model_config = ConfigDict(
         alias_generator=to_camel, populate_by_name=True, from_attributes=True
     )
+
+    @field_serializer("*")
+    def serialize_datetime(self, value: Any):
+        # 优先判断 datetime（因为它包含信息最全）
+        if isinstance(value, datetime):
+            return value.strftime("%Y-%m-%d %H:%M:%S")
+
+        # 再判断 date (仅年月日)
+        if isinstance(value, date):
+            return value.strftime("%Y-%m-%d")
+
+        return value
